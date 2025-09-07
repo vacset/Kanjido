a3110a25c8f9ee0b899035e04ad3e338f7d944b4
Vachirasuk Setalaphruk <v012345s@gmail.com>
2025-09-07 10:38:09 +0700

    chore: rename the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.seta.vacset.kanjido"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
